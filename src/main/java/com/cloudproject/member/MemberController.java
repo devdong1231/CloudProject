11df90fdda1e0ed165e20c9f953ec97ddddca169
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-05-20 13:51:32 +0900

    Feat: step-03 완료
    - s3 프로필 이미지 기능 추가

--- src/main/java/com/cloudproject/member/MemberController.java
+++ src/main/java/com/cloudproject/member/MemberController.java
@@ -3,11 +3,13 @@ package com.cloudproject.member;
 import com.cloudproject.member.dto.CreateMemberRequest;
 import com.cloudproject.member.dto.CreateMemberResponse;
 import com.cloudproject.member.dto.GetMemberResponse;
+import com.cloudproject.member.dto.ProfileImageResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,8 +24,24 @@ public class MemberController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<GetMemberResponse> getMember(@PathVariable Long id){
+    public ResponseEntity<GetMemberResponse> getMember(@PathVariable Long id) {
         GetMemberResponse result = memberService.getOneMember(id);
         return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @PostMapping("/{id}/profile-image")
+    public ResponseEntity<Void> uploadProfileImage(
+            @PathVariable Long id,
+            @RequestParam("image") MultipartFile image
+    ) {
+        memberService.uploadProfileImage(id, image);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/profile-image")
+    public ResponseEntity<ProfileImageResponse> getProfileImage(
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(memberService.getProfileImage(id));
     }
 }
